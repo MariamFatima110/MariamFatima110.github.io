541d7fce1a1e013336a76b0d29b01b53a808fac9
MariamFatima110 <mariam.fatima1@dcmail.ca>
2026-04-09 14:54:44 -0400

    Update javascript.js
    Finished js file before putting it into the validator.

--- assignment4/part1/javascript.js
+++ assignment4/part1/javascript.js
@@ -40,13 +40,16 @@ function generateStory() {
     newStory = newStory.replace("Bob", customName.value);
   }
 
+
+
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = Math.round(300 * 0.0714286) + " stone";
+    const temperature = Math.round((94 - 32) * (5 / 9)) + " centigrade";
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
-
